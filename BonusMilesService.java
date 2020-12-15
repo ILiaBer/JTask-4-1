aaf57a0dec3b9109a35114213eca9b0503768322
ILia Berestov <berestov_2000@bk.ru>
2020-12-15 16:51:23 +0300

    Second commit

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,7 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int amount = 6600;
-        int miles = amount / 20;
+        int miles = cost / 20;
         return miles;
     }
 }
--- Main41.java
+++ Main41.java
@@ -1,7 +1,7 @@
 public class Main41 {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int miles = service.calculate(6600);
+        int miles = service.calculate(8000);
         System.out.println(miles);
     }
 }
